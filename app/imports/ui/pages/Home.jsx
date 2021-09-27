17c0171ac192b9eb036a43ceb2c012d50157a7a1
jkulka <jkulka@hawaii.edu>
2021-09-26 19:56:46 -1000

    Brought status list up into the home page, small linking change in UpdateStatus

--- app/imports/ui/pages/Home.jsx
+++ app/imports/ui/pages/Home.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
-import { Grid, Segment, Button } from 'semantic-ui-react';
+import { Grid, Segment, Button, Header, Table, Container } from 'semantic-ui-react';
 import { NavLink } from 'react-router-dom';
 import { Meteor } from 'meteor/meteor';
+import { withTracker } from 'meteor/react-meteor-data';
+import PropTypes from 'prop-types';
+import { Status } from '../../api/status/Status';
+import StatusItem from '../components/StatusItem';
 
 /** A simple static component to render some text for the home page. */
 class Home extends React.Component {
@@ -12,12 +16,43 @@ class Home extends React.Component {
           <Segment>
             <h1>Health Check-in</h1>
             <Button primary as={NavLink} exact to='/update'>Update Symptoms</Button>
-            <Button secondary as={NavLink} exact to='/list'>Previous Check-ins</Button>
           </Segment>
+          <Container>
+            <Header as="h2" textAlign="center">Previous Check-ins</Header>
+            <Table celled>
+              <Table.Header>
+                <Table.Row>
+                  <Table.HeaderCell>Date</Table.HeaderCell>
+                  <Table.HeaderCell>Condition</Table.HeaderCell>
+                </Table.Row>
+              </Table.Header>
+              <Table.Body>
+                {this.props.status.map((status) => <StatusItem key={status._id} status={status} />)}
+              </Table.Body>
+            </Table>
+          </Container>
         </Grid.Column>
       </Grid>
     );
   }
 }
 
-export default Home;
+// Require an array of Status documents in the props.
+Home.propTypes = {
+  status: PropTypes.array.isRequired,
+  ready: PropTypes.bool.isRequired,
+};
+
+// withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker
+export default withTracker(() => {
+  // Get access to Stuff documents.
+  const subscription = Meteor.subscribe(Status.userPublicationName);
+  // Determine if the subscription is ready
+  const ready = subscription.ready();
+  // Get the Stuff documents
+  const status = Status.collection.find({}).fetch();
+  return {
+    status,
+    ready,
+  };
+})(Home);
