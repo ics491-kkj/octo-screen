5d131cecfdc859dc55656e794b33bab30050da7b
Kason Shiroma <kasons@hawaii.edu>
2021-09-26 19:08:54 -1000

    added background photo to home page

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,32 +1,37 @@
 import React from 'react';
-import { Grid, Segment, Button } from 'semantic-ui-react';
+import { Grid, Segment, Button, Icon, Container, Card } from 'semantic-ui-react';
 import { NavLink } from 'react-router-dom';
 import { Meteor } from 'meteor/meteor';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
   render() {
+    const segmentStyle = {
+      marginTop: '70px',
+      borderRadius: '15px',
+    };
     return (
-      <Grid id='landing-page' verticalAlign='middle' textAlign='center' container>
-
-        <Grid.Column width={8}>
-          {Meteor.userId() == null ? (
-            <Segment>
-              <h1>Welcome to Octo Screen!</h1>
-              <Button color='green' as={NavLink} exact to='/signin'> Sign in </Button>
-              <Button as={NavLink} exact to='/signup'> Register </Button>
-            </Segment>
-          ) : (
-            <Segment>
-              <h1>Health Check-in</h1>
-              <Button primary as={NavLink} exact to='/update'>Update Symptoms</Button>
-              <Button secondary as={NavLink} exact to='/list'>Previous Check-ins</Button>
-            </Segment>
-          )}
-
-        </Grid.Column>
-
-      </Grid>
+      <div id="background-image">
+        <Container>
+          <Grid verticalAlign='middle' textAlign='center' container>
+            <Grid.Column width={8}>
+              {Meteor.userId() == null ? (
+                <Card style={segmentStyle}>
+                  <h1>Welcome to Octo Screen!</h1>
+                  <Button as={NavLink} exact to='/signin'> Sign in </Button>
+                  <Button as={NavLink} exact to='/signup'> Register </Button>
+                </Card>
+              ) : (
+                <Card style={segmentStyle}>
+                  <h1>Health Check-in</h1>
+                  <Button primary as={NavLink} exact to='/update'>Update Symptoms</Button>
+                  <Button secondary as={NavLink} exact to='/list'>Previous Check-ins</Button>
+                </Card>
+              )}
+            </Grid.Column>
+          </Grid>
+        </Container>
+      </div>
     );
   }
 }
